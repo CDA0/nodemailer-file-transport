df052a4710a40ea7620b08c3b166babfcd7c53a7
Craig Anderson <craig.anderson@wipro.com>
2017-05-26 13:17:20 +0100

    add support for file extension and subject in filename

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ const packageData = require('./package.json');
 const FileTransport = function(options) {
   this.options = options || {};
   this.dir = options.dir || os.homedir();
+  this.ext = options.ext || null;
+  this.useSubject = options.useSubject || false;
   this.name = 'File';
   this.version = packageData.version;
 };
 
 FileTransport.prototype.send = function(mail, cb) {
-  fs.writeFileSync(path.join(this.dir, mail.data.to), mail.data.html);
+  var fname = mail.data.to;
+
+  if (this.useSubject) {
+    fname = `${fname} - ${mail.data.subject}`;
+  }
+
+  if (this.ext) {
+    fname = `${fname}.${this.ext}`;
+  }
+
+  fs.writeFileSync(path.join(this.dir, fname), mail.data.html);
   return cb();
 };
 
