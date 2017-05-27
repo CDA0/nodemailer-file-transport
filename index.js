af1942c4193cf0a924c766d3839380891af92ead
Craig Anderson <craig.anderson@wipro.com>
2017-05-27 10:58:04 +0100

    add support for arrays of destination addresses, add unit tests

--- index.js
+++ index.js
@@ -1,29 +1,73 @@
+'use strict';
+
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
+const userValidate = require('npm-user-validate');
 const packageData = require('./package.json');
 
 const FileTransport = function(options) {
   this.options = options || {};
-  this.dir = options.dir || os.homedir();
-  this.ext = options.ext || null;
-  this.useSubject = options.useSubject || false;
+  this.dir = this.options.dir || os.homedir();
+  this.ext = this.options.ext || null;
+  this.useSubject = this.options.useSubject || false;
+  this.sentMail = [];
   this.name = 'File';
   this.version = packageData.version;
 };
 
-FileTransport.prototype.send = function(mail, cb) {
-  var fname = mail.data.to;
+function validate (addr) {
+  try {
+    var err = userValidate.email(addr);
+    if (err != null) return err;
+  } catch (_err) {
+    return new Error('Error validating address.');
+  }
+  return null;
+}
+
+FileTransport.prototype.createFilename = function(to, subject) {
+  let fname = to;
 
   if (this.useSubject) {
-    fname = `${fname} - ${mail.data.subject}`;
+    fname = `${fname} - ${subject}`;
   }
 
   if (this.ext) {
     fname = `${fname}.${this.ext}`;
   }
 
-  fs.writeFileSync(path.join(this.dir, fname), mail.data.html);
+  return fname;
+};
+
+FileTransport.prototype.send = function(mail, cb) {
+  let fname;
+  let err;
+
+  if (!mail.data.to) {
+    return cb(new Error('No destination address'));
+  }
+
+  if (Array.isArray(mail.data.to)) {
+    for (var i = 0; i < mail.data.to.length; i++) {
+      fname = this.createFilename(mail.data.to[i], mail.data.subject);
+      err = validate(fname);
+      if (err) {
+        return cb(err);
+      }
+      fs.writeFileSync(path.join(this.dir, fname), mail.data.html);
+    }
+  } else {
+    fname = this.createFilename(mail.data.to);
+    err = validate(fname);
+    if (err) {
+      return cb(err);
+    }
+    fs.writeFileSync(path.join(this.dir, fname), mail.data.html);
+  }
+
+  this.sentMail.push(mail);
+
   return cb();
 };
 
